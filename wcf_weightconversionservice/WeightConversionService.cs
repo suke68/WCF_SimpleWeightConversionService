9e08378f07e0dafb89a1871e86b75250b018a320
Frank J Castro <frankjtc@gmail.com>
2014-12-14 18:21:43 -0800

    first commit

---
+++ wcf_weightconversionservice/WeightConversionService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_WeightConversionService
+{
+    public class WeightConversionService : IWeightConversionService
+    {
+        public double PoundsToKilos(double pounds)
+        {
+            return pounds / 2.2046226218;
+        }
+
+        public double KilosToPounds(double kilos)
+        {
+            return kilos * 2.2046226218;
+        }
+    }
+}
